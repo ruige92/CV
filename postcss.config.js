ef6fa0b35f1284754895d2737e936d6b7ee7697b
Ray Feng <ray.feng@cordantgroup.com>
2020-03-25 23:57:19 +0000

    modified public folder

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./docs/*.html"],
+  content: ["./public/*.html"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/.]+/g) || []
 });
 
